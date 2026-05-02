e757ea7e7f2cd8435807473cfdaea958baec649c
Ali Mashar <gold.harumaki@gmail.com>
2026-05-02 20:49:40 +0900

    tambah kalkulator JavaScript

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var tombol = document.getElementById("hitung");
 tombol.onclick = function() {
   var angka1 = Number(document.getElementById("angka1").value);
   var angka2 = Number(document.getElementById("angka2").value);
-  var hasil = angka1 + angka2;
-  document.getElementById("hasil").textContent = "Hasil: " + hasil;
+  var total = angka1 + angka2;
+  document.getElementById("hasil").textContent = "Hasil: " + total;
 };
