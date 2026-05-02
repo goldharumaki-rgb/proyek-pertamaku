7ebf33413a83809023a00a41d0d3d3201ddd0c7a
Ali Mashar <gold.harumaki@gmail.com>
2026-05-02 20:43:46 +0900

    tambah kalkulator JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var tombol = document.getElementById("tombol");
+var tombol = document.getElementById("hitung");
 
 tombol.onclick = function() {
-  document.body.style.backgroundColor = "#333333";
-  tombol.textContent = "Warna Berubah!";
+  var angka1 = Number(document.getElementById("angka1").value);
+  var angka2 = Number(document.getElementById("angka2").value);
+  var hasil = angka1 + angka2;
+  document.getElementById("hasil").textContent = "Hasil: " + hasil;
 };
